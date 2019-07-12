84737f3e6ad2285d2c9b3174cf627b0dd28186bb
J. Nathan Allen <nateonmission@gmail.com>
2019-07-12 12:51:55 -0400

    worked on maven

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App
