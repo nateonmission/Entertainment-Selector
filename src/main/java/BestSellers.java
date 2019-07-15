9135dc00f7b75b2b2107cdfa50734c50c1b7f0ce
J. Nathan Allen <nateonmission@gmail.com>
2019-07-15 18:19:50 -0400

    cleaned up dir3

--- src/main/java/BestSellers.java
+++ src/main/java/BestSellers.java
@@ -1,6 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.List;
 
 // Create object from NYT Best Seller CSV
--- src/main/java/HitSongs.java
+++ src/main/java/HitSongs.java
@@ -1,6 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.List;
 
 // The object that represents the top song
